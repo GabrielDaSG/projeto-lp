feb2ca12b0c1285048fe430eef9f0bd7772894b6
gabriel <gabrieldasilva501@gmail.com>
2022-11-11 00:00:20 -0300

    commit

--- src/atividade01/Questao06.java
+++ src/atividade01/Questao06.java
@@ -1,25 +1,21 @@
-package atividade01;
+package Atividade01;
 
-import java.util.Scanner;
-
-// Programa vai ler o valor de uma conta e quantas pessoas vão pagar, depois retornará quanto cada um vai pagar.
+//programa para ler o valor de uma conta e quantas pessoas vão dividir a conta, o sistema vai apresentar quanto cada pessoa vai pagar.
 
 public class Questao06 {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao programa de divisão de contas!");
+        System.out.println("Olá, seja bem vindo");
         Scanner input = new Scanner(System.in);
-        
+       
         System.out.println("Insira o valor total dos produtos consumidos: ");
-        
         double valorTotal = input.nextDouble();
         double totalMaisGarcom = valorTotal*1.1;
-        
+       
         System.out.println("O valor total, com os 10 percento do garçom é: " + totalMaisGarcom);
         System.out.println("Digite a quantidade de pessoas que vão dividir a conta: ");
-        
         double quantidadePessoas = input.nextDouble();
         double valorFinal = totalMaisGarcom/quantidadePessoas;
-        
+       
         System.out.println("O valor final a ser pago por cada um é: " + valorFinal);
         System.out.println("Obrigado por ter utilizado o programa! Até a próxima!");
     }
